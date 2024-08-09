d7eaa9526ebfc8d16bcd4ad25766c113c0972e40
Bohdan Yaroshepta <yaroshepta.b@gmail.com>
2024-08-10 00:06:27 +0300

    Fixed PDF viewer layout sizing

--- src/app/pdf-generator/components/viewer/PdfViewer.tsx
+++ src/app/pdf-generator/components/viewer/PdfViewer.tsx
@@ -8,7 +8,7 @@ export const PdfViewer = ({ base64Document }: PdfViewerProps) => {
     return (
         <div className='h-[800px]'>
             <PDFViewer
-                canvasCss='w-full flex justify-center rounded-xl'
+                canvasCss='w-full flex flex-col items-stretch rounded-xl'
                 key={base64Document}
                 document={{
                     base64: base64Document,
