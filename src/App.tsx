c55494745182537b9fdf683ca08aad2774c4f249
Bohdan Yaroshepta <yaroshepta.b@gmail.com>
2024-08-09 20:39:37 +0300

    Changed blue theme to black

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { PageContainer } from "./app/PageContainer";
 
 function App() {
     return (
-        <div className='w-full h-[100dvh] bg-blue-400'>
+        <div className='w-full h-[100dvh] bg-black'>
             <PageContainer />
         </div>
     );
