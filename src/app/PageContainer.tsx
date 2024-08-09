b70dbe6b7d39d0a2c772b09eee2e2f13aa35b5be
Bohdan Yaroshepta <yaroshepta.b@gmail.com>
2024-08-09 19:48:43 +0300

    Added app Banner to home page

---
+++ src/app/PageContainer.tsx
@@ -0,0 +1,9 @@
+import { Banner } from "./components/Banner";
+
+export const PageContainer = () => {
+    return (
+        <div className='w-full h-full flex flex-col justify-center items-center gap-4'>
+            <Banner />
+        </div>
+    );
+};
---
+++ src/app/components/Banner.tsx
@@ -0,0 +1,5 @@
+export const Banner = () => {
+    return (
+        <h1 className='text-neutral-100 text-[86px] font-semibold'>Fast PDF</h1>
+    );
+};
