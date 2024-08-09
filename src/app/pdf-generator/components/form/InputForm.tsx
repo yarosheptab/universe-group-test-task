d65da212902291f63bf682a49e5b5c996e90ca5d
Bohdan Yaroshepta <yaroshepta.b@gmail.com>
2024-08-10 00:03:18 +0300

    Added main logic to Input Form to enter and generate PDF files

--- src/app/pdf-generator/components/form/InputForm.tsx
+++ src/app/pdf-generator/components/form/InputForm.tsx
@@ -1,21 +1,65 @@
 import { useState } from "react";
+import { Spinner } from "../../../../shared/components/Spinner";
+import { convertTextToBase64PDF } from "../../../../utils/helpers";
 
-export const InputForm = () => {
+export interface InputFormProps {
+    onConvert: (content: string) => void;
+}
+
+export const InputForm = ({ onConvert }: InputFormProps) => {
     const [inputValue, setInputValue] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [convertError, setConvertError] = useState<string | null>(null);
+
+    const handleFormSubmit = async (e?: React.FormEvent<HTMLFormElement>) => {
+        e?.preventDefault();
+
+        if (!inputValue) {
+            return;
+        }
+        setIsLoading(true);
+        setConvertError(null);
+
+        const result = await convertTextToBase64PDF(inputValue);
+
+        if (result.status === "success") {
+            onConvert(result.result);
+            setInputValue("");
+        } else {
+            setConvertError(result.error);
+        }
+
+        setIsLoading(false);
+    };
     return (
         <form
             className='flex flex-col items-stretch gap-3'
-            onSubmit={(e) => e.preventDefault()}
+            onSubmit={handleFormSubmit}
         >
             <textarea
+                disabled={isLoading}
                 value={inputValue}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter" && !e.shiftKey) {
+                        e.preventDefault();
+                        handleFormSubmit();
+                    }
+                }}
                 onChange={(e) => setInputValue(e.target.value)}
                 placeholder='Введіть текст...'
-                className='resize-none w-[700px] h-[300px] w-max-[100%] p-8 bg-black border-2 border-neutral-500 text-lg text-neutral-100 outline-none focus-visible:ring-2 ring-offset-background focus-visible:ring-offset-2 rounded-2xl placeholder:text-neutral-600'
+                className='resize-none w-[700px] h-[300px] w-max-[100%] p-8 bg-black border-2 border-neutral-500 disabled:border-neutral-800 text-lg text-neutral-100 disabled:text-neutral-500 outline-none focus-visible:ring-2 ring-offset-background focus-visible:ring-offset-2 rounded-2xl placeholder:text-neutral-600'
             />
-            <button className='w-full bg-neutral-100 text-neutral-950 hover:bg-neutral-100/90 transition-all h-11 rounded-lg px-8 font-semibold'>
-                Конвертувати в PDF
+            <button
+                disabled={isLoading}
+                className='w-full bg-neutral-100 text-neutral-950 disabled:bg-neutral-400 enabled:hover:bg-neutral-100/90 transition-all h-11 rounded-lg px-8 font-semibold flex justify-center items-center'
+            >
+                {isLoading ? (
+                    <Spinner className='h-8 w-8' />
+                ) : (
+                    "Конвертувати в PDF"
+                )}
             </button>
+            {convertError && <p className='text-red-500'>{convertError}</p>}
         </form>
     );
 };
