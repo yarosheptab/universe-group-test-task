55af5829a45b1a378c9cd636eca0391dc680a53b
Bohdan Yaroshepta <yaroshepta.b@gmail.com>
2024-08-10 00:04:50 +0300

    Connected Input Form functionality with PDF viewer component

--- src/app/pdf-generator/PdfGeneratorContainer.tsx
+++ src/app/pdf-generator/PdfGeneratorContainer.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { InputForm } from "./components/form/InputForm";
+import { PdfViewer } from "./components/viewer/PdfViewer";
 
 export const PdfGeneratorContainer = () => {
+    const [openedDocument, setOpenedDocument] = useState<string | null>(null);
+
+    const handlePdfCreate = async (base64Content: string) => {
+        setOpenedDocument(base64Content);
+    };
+
     return (
         <div className='flex flex-col gap-4'>
-            <InputForm />
+            <InputForm onConvert={handlePdfCreate} />
+            {openedDocument && <PdfViewer base64Document={openedDocument} />}
         </div>
     );
 };
