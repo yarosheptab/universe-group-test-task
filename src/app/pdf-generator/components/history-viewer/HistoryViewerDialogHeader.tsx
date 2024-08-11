b99839f31f04d6b88a3911002f5c66af14c2cc02
Bohdan Yaroshepta <yaroshepta.b@gmail.com>
2024-08-11 13:21:51 +0300

    Added HistoryViewerDialogHeader component

---
+++ src/app/pdf-generator/components/history-viewer/HistoryViewerDialogHeader.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export interface HistoryViewerDialogHeaderProps {
+    children?: ReactNode;
+}
+
+export const HistoryViewerDialogHeader = ({
+    children,
+}: HistoryViewerDialogHeaderProps) => {
+    return <div className='flex flex-row justify-between'>{children}</div>;
+};
